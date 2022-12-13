51e83671cb01c1e47e6d7b060f9f56aded159901
Damiano <damybert@gmail.com>
2022-12-13 15:09:10 +0100

    da linux vecchie cose e immagine dept map

--- prova/main.py
+++ prova/main.py
@@ -14,6 +14,29 @@ def callback(data):
     i = 0
     j = 0
 
+    image_arr2 = [[] for l in range(int(tot_data/(640*3)))]
+    k = 0
+    m = 0
+    while m < tot_data / (480*3):
+        k = 0
+        while k < tot_data / (640*3):
+            image_arr2[k].append(struct.unpack_from('3B', data.data, int(k*3 + m * tot_data / 640)))
+            k += 1
+
+        m+=1
+
+    print("creating asarray")
+
+    arr = np.asarray(image_arr2, dtype='uint8')
+    print("creating pilimage")
+
+    img = pilimage.fromarray(arr)
+    print("saving")
+
+    img.save('new.png')
+    print("done")
+
+
     image_arr = []
     while i < tot_data / (640*3):
         riga = []
@@ -47,7 +70,7 @@ def callback(data):
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/ee_camera/camera/rgb/image_raw" , Image, callback)
+    rospy.Subscriber("/z_base_camera/camera/rgb/image_raw" , Image, callback)
 
     rospy.spin()
 
