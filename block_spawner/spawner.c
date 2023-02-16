d3e11021e4e3064c7b10616e2344eb61f4a4fc81
Matteo Beltrami <71525176+matteobeltrami@users.noreply.github.com>
2023-02-16 01:52:09 -0800

    update with task 3

--- block_spawner/spawner.c
+++ block_spawner/spawner.c
@@ -6,10 +6,12 @@
 
 #define LINE_NUMEBR 24
 
+//int write_at_line(int n_block);
 int randomInt(int a, int b);
 float randomFloat(float lower_bound, float upper_bound);
 void task1();
 void task2(int n_blocks);
+void task3(int n_blocks);
 void open_files();
 void close_files();
 int checkDuplicates(int already[], int n, int proposal);
@@ -36,7 +38,7 @@ int main(int argc, char *argv[]) {
       task2(n_blocks);
       break;
     case 3:
-      //task(n_blocks, 3);
+      task3(n_blocks);
       break;
     case 4:
       //task(n_blocks, 4);
@@ -165,6 +167,90 @@ void task2(int n_blocks) {
   fclose(file2);
 
   printf("world for task2 created with %d blocks:", n_blocks);
+  for(int i=0; i<n_blocks; i++){printf(" %d", taken[i]);} printf("\n");
+  return;
+}
+
+void task3(int n_blocks) {
+
+  char line[100];
+  char *str = "This is a string to append";
+
+  int current_line = 1;
+
+  FILE *file = fopen("../ros_impedance_controller/worlds/mio_tavolo_temp.world", "r+");
+  FILE *file2 = fopen("../ros_impedance_controller/worlds/mio_tavolo.world", "w");
+
+  if (file == NULL) {
+    printf("Error opening file!\n");
+    return;
+  }
+
+  if (file2 == NULL) {
+    printf("Error opening file 2!\n");
+    return;
+  }
+
+  int taken[n_blocks];
+  for(int j = 0; j < n_blocks; j++){
+    taken[j] = -1;
+  }
+
+  while (fgets(line, sizeof(line), file)) {
+    fprintf(file2, "%s", line);
+
+    if (current_line == LINE_NUMEBR) {
+      // seek back to the beginning of the line
+      fseek(file, -strlen(line), SEEK_CUR);
+
+      for (int i = 0; i < n_blocks; i++) {
+        int block_num = randomInt(0, 10);
+        taken[i] = block_num;
+        int pose = randomInt(1, 4);
+        
+        float x = randomFloat(0.05, 0.6);
+        float y = randomFloat(0.22, 0.75);
+        float z = 0.871;
+        float r = 0.0;
+        float p = 0.0;
+        float ya = randomFloat(0.0, 6.28318);
+        switch(pose){
+          case 1:
+            r = 0.0;
+            p = 0.0;
+            break;
+          case 2:
+            r = 1.57079;
+            p = 0.0;
+            break;
+          case 3:
+            r = 1.57079;
+            p = 1.57079;
+            break;
+          case 4:
+            r = 3.14159;
+            z += 0.2;
+            break;
+        }
+
+        // write the line, including the string to append
+        fprintf(file2, "%s""%d""%s""%d""%s""%.5f"" %.5f"" %.5f"" %.5f"" %.5f"" %.5f""%s", "    <include>\n     <name>block#", i+1, "</name>\n     <uri>model://block", block_num, "high</uri>\n     <pose>", x, y, z, r, p, ya, "</pose>\n    </include>\n\n");
+      }
+      
+      break;
+    }
+    current_line++;
+  }
+
+  fprintf(file2, "%s",
+          "    <gui>\n     <camera name=\"gzclient_camera\">\n     <pose>1. "
+          "3.2 2.2 0. 0.4 -1.75</pose>\n     </camera>\n    </gui>\n\n  "
+          "</world>\n</sdf>");
+
+  fclose(file);
+  fclose(file2);
+
+  printf("world for task3 created with %d blocks:", n_blocks);
   for(int i=0; i<n_blocks; i++){printf(" %d", taken[i]);} printf("\n");
   return;
 }
